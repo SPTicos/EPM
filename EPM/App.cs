d301bcfcdd5fa8c2a222503d5fc40e630fdb0b99
Pavel Espitia <pavelespitia@gmail.com>
2014-12-24 10:53:43 -0600

    Initial Version -Fixed

---
+++ EPM/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace EPM
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new EPM.MainPage ();
+		}
+	}
+}
+
---
+++ EPM/Pages/testPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EPM
+{	
+	public partial class testPage : ContentPage
+	{	
+		public testPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
