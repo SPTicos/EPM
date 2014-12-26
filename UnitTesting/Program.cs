770bcc28e31bca0a698167851ae7b81fa0df11f3
Carlos Rene <ccerrato147@gmail.com>
2014-12-26 11:29:19 -0600

    added mockup service
    Service to display the options of the application

---
+++ UnitTesting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using EPM.Services;
+
+namespace UnitTesting
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
