e6737f510ca90307e5ad97c98ede0f86c2d87a8a
Pavel Espitia <pavelespitia@gmail.com>
2014-12-29 15:26:55 -0600

    MainPage Layout
    Layout Updated

--- Android/obj/Debug/android/src/epm/android/R.java
+++ Android/obj/Debug/android/src/epm/android/R.java
@@ -11,8 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int monoandroidsplash=0x7f020001;
+        public static final int accountcircle=0x7f020000;
+        public static final int airplane=0x7f020001;
+        public static final int alarmmultiple=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int landscape=0x7f020004;
+        public static final int monoandroidsplash=0x7f020005;
     }
     public static final class id {
         public static final int myButton=0x7f060000;
