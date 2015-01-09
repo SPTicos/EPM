d6965481171a7e5408dd96b213a0566f7455de3d
Carlos Rene <ccerrato147@gmail.com>
2015-01-09 10:54:48 -0600

    Reparación después de error critico!

--- EPM/App.cs
+++ EPM/App.cs
@@ -7,8 +7,8 @@ namespace EPM
 	{
 		public static Page GetMainPage ()
 		{	
-			//return new NavigationPage (new MainPage ());
-			return new NavigationPage (new VacationApproval ());
+			return new NavigationPage (new MainPage ());
+			//return new NavigationPage (new VacationApproval ());
 		}
 
 		public static INavigation Navigator { get; set; }
