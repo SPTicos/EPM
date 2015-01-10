a7051290b296840d859295f8bc0c673749a5ddd1
Pavel Espitia <pavelespitia@gmail.com>
2015-01-10 11:49:36 -0600

    Vacation History Completed

---
+++ EPM/Pages/RequestHistory.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EPM
+{	
+	public partial class RequestHistory : ContentPage
+	{	
+		public RequestHistory ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ EPMRepositories/Entities/VacationHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPMRepositories
+{
+	public class VacationHistory
+	{
+		public VacationHistory ()
+		{
+		}
+	}
+}
+
