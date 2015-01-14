e1bf842606d1a21009cddd0be9769f4d0cb666a6
Carlos Rene <ccerrato147@gmail.com>
2015-01-14 02:07:41 -0600

    Commit before merge trying to fix error

---
+++ EPMRepositories/Entities/VacationHistory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EPMServices
+{
+	public class VacationHistory
+	{
+
+		public User User {
+			get;
+			set;
+		}
+
+		public DateTime InitialDate {
+			get;
+			set;
+		}
+
+		public DateTime EndDate {
+			get;
+			set;
+		}
+
+		public string Duration {
+			get;
+			set;
+		}
+
+		public string DateRange {
+			get;
+			set;
+		}
+
+		public VacationHistory ()
+		{
+		}
+	}
+}
+
